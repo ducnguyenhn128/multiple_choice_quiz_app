d620be7a89bf049c7258bbedcf9046599ca186f6
ducnguyenhn128 <116652871+ducnguyenhn128@users.noreply.github.com>
2023-09-21 10:28:44 +0700

    update review later question

--- quizapp/src/Page/OfficialTest/OfficialTest.jsx
+++ quizapp/src/Page/OfficialTest/OfficialTest.jsx
@@ -44,12 +44,24 @@ const OfficialTest = () => {
         // navigate('/officialtest/result')
     }
 
+    // user select their choice 
     const handleChoiceChange = (questionIndex, choiceIndex) => {
         const newAnswer = [...UserAnswer];
         newAnswer[questionIndex] = choiceIndex;
         setUserAnswer(newAnswer)
     }
 
+    // user review their question later
+    const toggleReviewLater = (element) => {
+        const currentClassName = element.className;
+        if (currentClassName.includes('review_later')) {
+          // Remove 'review_later' from the className
+          element.className = currentClassName.replace('review_later', '').trim();
+        } else {
+          // Add 'review_later' to the className
+          element.className = `${currentClassName} review_later`.trim();
+        }
+      };
 
     return (  
         <div className='official_test_page'>
@@ -86,16 +98,41 @@ const OfficialTest = () => {
                     </div>
                     <button onClick = {submitAction}>Submit yout test</button>
                     <p>Your test score is: {userScore}/10</p>
+                    
 
+                    {/* List of question summary */}
                     <div className='official_test_page_summary_question'>
                         {questionBank.map((el, questionIndex) => (
                             <div 
-                                key={questionIndex}
-                                className={`official_test_page_summary_question_item ${UserAnswer[questionIndex] === null ? 'question_item_not_done' : 'question_item_done'}`} 
+                            key={questionIndex}
+                            className={`official_test_page_summary_question_item ${UserAnswer[questionIndex] === null ? 'question_item_not_done' : 'question_item_done'}`} 
+                            onClick={(e) => toggleReviewLater(e.currentTarget)}
                             >
                                 {questionIndex + 1}
                             </div>
                         ))}
+                    </div>
+
+                    {/* Detail */}
+                    <div className='question_test_page_summary_detail'>
+                        <div style={{display:'flex'}}>
+                            <div 
+                                className='official_test_page_summary_question_item question_item_done'>
+                            </div>
+                            <p>Question answered</p>
+                        </div>
+                        <div style={{display:'flex'}}>
+                            <div 
+                                className='official_test_page_summary_question_item question_item_not_done'>
+                            </div>
+                            <p>Question has not answered</p>
+                        </div>
+                        <div style={{display:'flex'}}>
+                            <div 
+                                className='official_test_page_summary_question_item question_item_done review_later'>
+                            </div>
+                            <p>Question need to be review later</p>
+                        </div>
                     </div>
                 </div>
 
